f51c8a26739ab17c5fd2c7848a686e11ae4cac17
Jordan Cozart <Jordan.Cozart@matrixteam.com>
2022-08-05 15:21:52 -0500

    Completed BlazerClientApp

--- BlazorApiClient/Program.cs
+++ BlazorApiClient/Program.cs
@@ -1,7 +1,11 @@
+using BlazorApiClient.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<TokenModel>();
+
 builder.Services.AddHttpClient("api", opts =>
 {
     opts.BaseAddress = new Uri(builder.Configuration.GetValue<string>("ApiUrl"));
